531f0a937365417bf35a5b7c0783a2801a172f53
juanmanuelrmarano <juanmanuelrmarano@gmail.com>
2021-09-28 15:40:21 -0300

    Update mongo.py

--- handle/mongo.py
+++ handle/mongo.py
@@ -5,7 +5,7 @@ class Mongo():
         self.client = None
 
     def connect(self):
-        self.client = MongoClient(host='mongodb+srv://suika:VolkswaguenGol2001@suikadata.rerjq.mongodb.net/admin?replicaSet=atlas-vb3vsy-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1')
+        self.client = MongoClient(host='')
         return self.client
 
     def get_db(self, db='suika_api'):
